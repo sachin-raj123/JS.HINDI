b99d7cafdca787fb5d54ad858c5a8483ac1183fc
sachin-raj123 <sachinraj9986@gmail.com>
2025-09-04 20:34:31 +0530

    save changes

---
+++ 01_variables.js
@@ -0,0 +1,35 @@
+const accountId = 123456;
+let accountEmail = "sachin@google.com";
+var accountPassword = "12345";
+accountCity = "Bangalore";
+
+//accountId = 2; // not allowed
+accountEmail = "hc@hc.com"
+accountPassword = "abcd"
+accountCity = "New York"
+
+console.log(accountId);
+console.table([ accountId, accountEmail, accountPassword, accountCity]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
